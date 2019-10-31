382541393ed0dc69febeef5454c111c83e2b0bc2
Sergei Sumarokov <sumarokovsergei@users.noreply.github.com>
2019-10-31 12:54:44 +0300

    Added main changes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,49 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 10000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150}
-];
+class ProductList {
+    constructor(container = '.products') {      // В конструктор добавляем контейнер, хранящий селектор по которому будем делать выборку
+        this.container = container;
+        this.goods = [];                // Массив товаров, который придет с сервера
+        this.allProducts = [];         // Массив, который будет уже хранить всю разметку, с уже сгенерироваными товарами
+        this._fetchProducts();
+        this._render();
+    }
+
+    _fetchProducts() {                  // Инкапсулирвоаный метод
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 10000},
+            {id: 2, title: 'Mouse', price: 100},
+            {id: 3, title: 'Keyboard', price: 250},
+            {id: 4, title: 'Gamepad', price: 150, img: ''}
+        ];
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);     // Получим блок для рендера, куда будем помещать товары
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
 
 
-const renderProduct = (title="Товар", price="1997") => {    //Создает карточку товара, добавлены значения переменным по умолчанию
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить</button>
-            </div>`;
-};
+class ProductItem {
+    constructor(product, img='https://placehold.it/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
 
-const renderProducts = lst => {
-    const productList = lst.map(item => renderProduct(item.title, item.price)).join('');    //.join('') объединяет строку разделеную пробелами
-    document.querySelector('.products').innerHTML = productList;
-};
+    // Метод отвечающий за рендер нашей разметки
+    render() {
+        return `<div class="product-item">
+                    <h3>${this.title}</h3>
+                    <img src="${this.img}" alt="">
+                    <p>${this.price} ₽</p>
+                    <button class="by-btn">Добавить</button>
+                </div>`;
+    }
+}
 
-renderProducts(products);
+const list = new ProductList();
